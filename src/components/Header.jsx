4dfd8e440a1e73be5dd0f3b0dee016158a462c6f
kir-gek <kirokronos@gmail.com>
2023-09-22 20:46:02 +0300

    initial

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://i.pinimg.com/originals/c5/b7/52/c5b752cc0bf052877e4057d6cb4d7203.png' />
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+const Profile = () => {
+    return (
+        <div className='content'>
+        <img src='https://yahont-hotel.ru/ckeditor_images/maxresdefault.jpg' />
+        <div>
+          Ava + description
+        </div>
+        <div>
+          My post
+          <div>
+            New post
+          </div>
+          <div>
+            <div>Post1</div>
+            <div>Post2</div>
+          </div>
+        </div>
+      </div>
+    )
+}
+
+export default Profile;
