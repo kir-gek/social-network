126677673dac570d6a8c905aa7352e38843912a9
kir-gek <kirokronos@gmail.com>
2023-09-29 21:28:02 +0300

    filled out <Dialogs />

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,41 @@
 import React from "react";
 import s from './Dialogs.module.css'
+
 const Dialogs = () => {
     return (
-      <div>
-          Dialogs
-      </div>
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog}>
+                    Pasha
+                </div>
+                <div className={s.dialog}>
+                    Masha
+                </div>
+                <div className={s.dialog  + ' ' + s.active}>
+                    Marina
+                </div>
+                <div className={s.dialog}>
+                    Danila
+                </div>
+                <div className={s.dialog}>
+                    Bob
+                </div>
+                <div className={s.dialog}>
+                    Alex
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>
+                    Hi
+                </div>
+                <div className={s.message}>
+                    Hello? how are you?
+                </div>
+                <div className={s.message}>
+                    Yo
+                </div>
+            </div>
+        </div>
     );
 }
 
